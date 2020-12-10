abb880c7174ac2280bd57b4fcd3dc735fc6f4723
Matt Mitchell <matt.mitchell@nrel.gov>
2020-12-10 13:09:02 -0700

    add setup, initial structuring

---
+++ setup.py
@@ -0,0 +1,26 @@
+import codecs
+from pathlib import Path
+
+from setuptools import setup, find_packages
+
+from enstore import VERSION
+
+this_dir = Path(__file__).parent
+with codecs.open(this_dir / "README.MD", encoding="utf-8") as ld_file:
+    long_description = ld_file.read()
+
+with codecs.open(this_dir / "LICENSE", encoding="utf-8") as lic_file:
+    enstore_license = lic_file.read()
+
+setup(
+    name="EnStoreRefactorPrototype",
+    author="Matt Mitchell",
+    license=enstore_license,
+    long_description=long_description,
+    version=VERSION,
+    packages=find_packages(exclude=["test", "tests", "test.*"]),
+    long_description_content_type='text/markdown',
+    test_suite='nose.collector',
+    tests_require=['nose'],
+    python_requires=">=3.5"
+)
